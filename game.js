a511906e8e6decdca4bf81e6ecb3b17792cf9966
George Ascherl <georgea@cox.net>
2015-10-30 15:46:33 -0700

    added css questions to display red or green

--- game.js
+++ game.js
@@ -35,12 +35,15 @@ display.textContent = 'Does A.J. drive a Jeep Wrangler?';
     score += 1;
     answer1 = true;
     pic1.innerHTML = '<img src="jeep.jpg" alt="Jeep Wrangler" height="200">';
+    ans1.className = 'correct';
   } else if (q1.toLowerCase() === "no" || q1.toLowerCase() === "n" || q1.toLowerCase() === "nope"){
     ans1.innerHTML = "Incorrect! A.J. does drive a Jeep Wrangler";
     console.log('The user answered q1 incorrectly with a: ' + q1);
     answer1 = false;
+    ans1.className = 'incorrect';
   } else {
     ans1.innerHTML = "'" + q1 + "' is an invalid answer. Try again.";
+    ans1.className = 'incorrect';
     firstQuestion();
   }
 }
@@ -52,14 +55,17 @@ var secondQuestion = function(){
     ans2.innerHTML = "Incorrect! A.J. doesn't own any cats at all!";
     console.log('The user answered q2 incorrectly with a: ' + q2);
     answer2 = false;
+    ans2.className = 'incorrect';
   } else if (q2.toLowerCase() === "no" || q2.toLowerCase() === "n" || q2.toLowerCase() === "nope"){
     ans2.innerHTML = "Correct! A.J. doesn't own any cats at all!";
     console.log('The user answered q2 correctly with a: ' + q2);
     score += 1;
     answer2 = true;
+    ans2.className = 'correct';
     pic2.innerHTML = '<img src="nocats.jpg" alt="No Cat" height="200">';
   } else {
     ans2.innerHTML = "'" + q2 + "' is an invalid answer. Try again.";
+    ans2.className = 'incorrect';
     secondQuestion();
   }
 }
@@ -72,13 +78,16 @@ var thirdQuestion = function() {
     console.log('The user answered q3 correctly with a: ' + q3);
     score += 1;
     answer3 = true;
+    ans3.className = 'correct';
     pic3.innerHTML = '<img src="rbaby.jpg" alt="Rosemary\'s Baby" height="200">';
   } else if (q3.toLowerCase() === "no" || q3.toLowerCase() === "n" || q3.toLowerCase() === "nope"){
     ans3.innerHTML = "Incorrect! AJ loves that movie!";
     console.log('The user answered q3 incorrectly with a: ' + q3);
     answer3 = false;
+    ans3.className = 'incorrect';
   } else {
     ans3.innerHTML = "'" + q3 + "' is an invalid answer. Try again.";
+    ans3.className = 'incorrect';
     thirdQuestion();
   }
 }
@@ -90,14 +99,17 @@ var fourthQuestion = function() {
     ans4.innerHTML = "Incorrect! A.J. hates eggplant!";
     console.log('The user answered q4 incorrectly with a: ' + q4);
     answer4 = false;
+    ans4.className = 'incorrect';
   } else if (q4.toLowerCase() === "no" || q4.toLowerCase() === "n" || q4.toLowerCase() === "nope"){
     ans4.innerHTML = "Correct! A.J. hates eggplant!";
     console.log('The user answered q4 correctly with a: ' + q4);
     score += 1;
     answer4 = true;
+    ans4.className = 'correct';
     pic4.innerHTML = '<img src="noeggplant.jpg" alt="No Eggplant" height="200">';
   } else {
     ans4.innerHTML = "'" + q4 + "' is an invalid answer. Try again.";
+    ans4.className = 'incorrect';
     fourthQuestion();
   }
 }
@@ -108,17 +120,21 @@ function fifthQuestion() {
   while (parseInt(q5) !== 5) {
     if (q5 > 5){
       ans5.innerHTML = 'Incorrect! Your guess was too high. Try again';
+      ans5.className = 'incorrect';
       q5 = prompt("What number am I thinking?");
     } else if (q5 < 5) {
       ans5.innerHTML = 'Incorrect! Your guess was too low. Try again';
+      ans5.className = 'incorrect';
       q5 = prompt("What number am I thinking?");
     } else {
       ans5.innerHTML = 'Incorrect! You entered an invalid number. Please try again.';
+      ans5.className = 'incorrect';
       q5 = prompt("What number am I thinking?");
     }
   }
   ans5.innerHTML = "You guessed the correct number of 5!";
   score += 1;
+  ans5.className = 'correct';
   pic5.innerHTML = '<img src="5.jpg" alt="Number" height="200">';
 }
 
@@ -128,17 +144,21 @@ function sixthQuestion() {
   while(parseInt(q6) !== 1987) {
     if (q6 > 1987){
       ans6.innerHTML = 'How flattering! Unfortunately, you guessed incorrectly! A.J. was born before the year ' + q6 +'. Try again.';
+      ans6.className = 'incorrect';
       q6 = prompt("What year was A.J. born?");
     } else if (q6 < 1987) {
       ans6.innerHTML = 'How dare you! A.J. was born after the year ' + q6 +'. Try again.';
+      ans6.className = 'incorrect';
       q6 = prompt("What year was A.J. born?");
     } else {
       ans6.innerHTML = 'Incorrect! You entered an invalid number.  Please try again.';
+      ans6.className = 'incorrect';
       q6 = prompt("What year was A.J. born?");
     }
   }
   ans6.innerHTML = "You guessed correctly! A.J. was born in 1987!";
   score += 1;
+  ans6.className = 'correct';
   pic6.innerHTML = '<img src="1987.jpg" alt="Birth Year" height="200">';
 }
 
